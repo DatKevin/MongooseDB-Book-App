9425b63b711117123daace3e6dd0877d1f15fd86
Kevin Dat Nguyen <datkevin@gmail.com>
2019-11-25 10:11:25 -0800

    Added CRUD methods

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 //require express in our app
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require("./models")
 
 // generate a new express app and call it 'app'
 const app = express();
 
-//---------------------------------MIDDLEWARE
+//--------------------------------he -MIDDLEWARE
 
 // serve static files in public
 app.use(express.static('public'));
@@ -19,30 +20,6 @@ const PORT = process.env.PORT || 3000;
 
 //----------------------------- TEMP DATA
 
-const books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
-
 
 let newBookUUID = 18;
 
@@ -53,66 +30,76 @@ app.get('/',  (req, res) => {
   res.sendFile('views/index.html' , { root : __dirname});
 });
 
-// get all books
-app.get('/api/books',  (req, res) => {
-  // send all books as JSON response
-  console.log('books index');
-  res.json(books);
-});
+//get all books
+app.get("/api/books", (req,res) => {
+  db.BookModel.find((err, books) => {
+    if (err) {
+      console.log("Get all books error: " + err)
+      res.sendStatus(500)
+    }
+    res.json(books)  
+  })
+})
+
 
 // get one book
 app.get('/api/books/:id',  (req, res) => {
   // find one book by its id
-  console.log('books show', req.params);
-  for(let i=0; i < books.length; i++) {
-    if (books[i]._id === req.params.id) {
-      res.json(books[i]);
-      break; // we found the right book, we can stop searching
+  console.log('books show', req.params.id)
+  let findid = {_id: req.params.id}
+  db.BookModel.find(findid, (err, books) => {
+    if (err) {
+      console.log("Get single book error: ")
+      res.sendStatus(500)
     }
-  }
-});
+    res.json(books)
+  })
+})
 
 // create new book
+app.use(express.json())
+
 app.post('/api/books',  (req, res) => {
   // create new book with form data (`req.body`)
-  console.log('books create', req.body);
-  const newBook = req.body;
-  newBook._id = newBookUUID++;
-  books.push(newBook);
-  res.json(newBook);
-});
+  let body = req.body
+  console.log('books create', body);
+  db.BookModel.create(body, (err, books) => {
+    if (err) {
+      console.log("Create book error: ")
+      res.sendStatus(500)
+    }
+    res.json(body);
+  })
+})
 
 // update book
 app.put('/api/books/:id', (req,res) => {
 // get book id from url params (`req.params`)
   console.log('books update', req.params);
-  const bookId = req.params.id;
   // find the index of the book we want to remove
-  const updateBookIndex = books.findIndex((element, index) => {
-    return (element._id === parseInt(req.params.id)); //params are strings
-  });
-  console.log('updating book with index', deleteBookIndex);
-  const bookToUpdate = books[deleteBookIndex];
-  books.splice(updateBookIndex, 1, req.params);
-  res.json(req.params);
-});
+  db.BookModel.findOneAndUpdate({"_id": req.params.id}, req.body, (err) => {
+    if (err) {
+      console.log("update book error: ")
+      res.sendStatus(500)
+    }
+    res.send("updated")
+  })
+})
 
 // delete book
 app.delete('/api/books/:id',  (req, res) => {
   // get book id from url params (`req.params`)
-  console.log('books delete', req.params);
-  const bookId = req.params.id;
+  console.log('books delete', req.params)
   // find the index of the book we want to remove
-  const deleteBookIndex = books.findIndex((element, index) => {
-    return (element._id === parseInt(req.params.id)); //params are strings
-  });
-  console.log('deleting book with index', deleteBookIndex);
-  const bookToDelete = books[deleteBookIndex];
-  books.splice(deleteBookIndex, 1);
-  res.json(bookToDelete);
-});
+  db.BookModel.deleteOne({"_id": req.params.id}, (err) => {
+    if (err) {
+      console.log("delete book error: ")
+      res.sendStatus(500)
+    }
+    res.send("deleted")
 
-
+  })
+})
 
 
 // Start Server
